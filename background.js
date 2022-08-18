438c6f27acf4c0fd62d43ee9a4a5eec93925d3a7
FabijanC <fabijan.corak@gmail.com>
2022-08-19 00:47:32 +0200

    Use color coding (close #1)

--- background.js
+++ background.js
@@ -1,9 +1,26 @@
+/**
+ * Heuristicaly designed.
+ * Returns 255 - sqrt(1000 * languages) if > 0, otherwise 0
+ * 
+ * @param {number} languages number of languages
+ * @returns color tone calculated from the number of languages
+ */
+function languagesToTone(languages) {
+    return Math.round(Math.max(255 - Math.sqrt(1000 * languages), 0));
+}
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function(request, sender) {
         if (request.langNumber) {
             chrome.action.setBadgeText({
-                "tabId": sender.tab.id,
-                "text": request.langNumber
+                tabId: sender.tab.id,
+                text: request.langNumber
+            });
+
+            const tone = languagesToTone(request.langNumber);
+            chrome.action.setBadgeBackgroundColor({
+                tabId: sender.tab.id,
+                color: `rgb(200, ${tone}, 50)`
             });
         }
     }
